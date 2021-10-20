825d3848781cffce27931abde69cfc9616e548b7
Pavel Solokha <pavel.solokha@gmail.com>
2021-10-20 09:13:53 +0300

    edge driver testing

--- src/test/java/ru/yandex/market/Laptop.java
+++ src/test/java/ru/yandex/market/Laptop.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -18,11 +19,14 @@ public class Laptop {
     @Test
     public void test() {
         // Для Windows
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        //System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
         // Для Linux и Mac
         //System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+        //WebDriver driver = new ChromeDriver();
+        //Edge
+        System.setProperty("webdriver.edge.driver", "src/test/resources/drivers/msedgedriver.exe");
+        WebDriver driver = new EdgeDriver();
 
-        WebDriver driver = new ChromeDriver();
         wait = new WebDriverWait(driver, 100, 2000);
         driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
         
